b3d8f6ff8e8ebb57d1a1e4c441f07af84c56a618
Jiwo Kristi <jiwo.kristi@ist.id>
2022-10-12 14:09:42 +0700

    drawer and responsive header done

--- src/components/ui/Header/style.js
+++ src/components/ui/Header/style.js
@@ -4,9 +4,24 @@ export const useStyles = makeStyles((theme) => ({
   toolbarMargin: {
     ...theme.mixins.toolbar, // to add a cushion underneath the AppBar component following the height of the Toolbar component
     marginBottom: "3em", // to add an extra space to push out content from underneath the AppBar, because after the Toolbar had been handled we added the Logo component
+    [theme.breakpoints.down("md")]: {
+      marginBottom: "2em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      marginBottom: "1.25em",
+    },
+  },
+  appBar: {
+    zIndex: theme.zIndex.modal + 1,
   },
   logo: {
     height: "8em",
+    [theme.breakpoints.down("md")]: {
+      height: "7em",
+    },
+    [theme.breakpoints.down("xs")]: {
+      height: "5.5em",
+    },
   },
   logoContainer: {
     padding: 0,
@@ -38,6 +53,33 @@ export const useStyles = makeStyles((theme) => ({
     ...theme.typography.tab,
     opacity: 0.7,
     "&:hover": {
+      opacity: 1,
+    },
+  },
+  drawerIconContainer: {
+    marginLeft: "auto",
+    "&:hover": {
+      background: "transparent",
+    },
+  },
+  drawerIcon: {
+    width: "50px",
+    height: "50px",
+  },
+  drawer: {
+    background: theme.palette.common.blue,
+  },
+  drawerItem: {
+    ...theme.typography.tab,
+    color: "#FFFFFF",
+    opacity: 0.7,
+  },
+  drawerItemEstimate: {
+    background: theme.palette.common.orange,
+  },
+  drawerItemSelected: {
+    // this specifies that any list item text component within a selected list item component is going to have an opacity of 1 on its root class, which will get our opacity onto the text where we needed it
+    "& .MuiListItemText-root": {
       opacity: 1,
     },
   },
